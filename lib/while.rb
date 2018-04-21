63702474b90966c4ebf33e477e3521463fd77e0c
Leticia Molina <letimolina@gmail.com>
2018-04-21 23:45:32 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,7 +2,12 @@ def using_while
 	levitation_force = 6
 	while levitation_force < 10
 	puts ""
-	levitation_force+=1
+	levitation_force += 1
 end
 
 
+ counter = 0
+# while counter < 20
+#   puts "The current number is less #{counter}."
+#   counter += 1
+# end
